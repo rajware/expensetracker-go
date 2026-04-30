79542ebca49cc80c7c7daa676d41c49d14b682a9
Raj Chaudhuri <rajch@hotmail.com>
2026-04-30 15:57:55 +0530

    Go project initialized

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/rajware/expensetracker-go
+
+go 1.25.1
---
+++ invoke.build.ps1
@@ -0,0 +1,14 @@
+Param(
+    $VersionMajor   = (property VERSION_MAJOR "1"),
+    $VersionMinor   = (property VERSION_MINOR "0"),
+    $BuildNumber    = (property BUILD_NUMBER  "0"),
+    $PatchString    = (property PATCH_NUMBER  "-alpha1"),
+    $RegistryUser   = (property REGISTRY_USER "quay.io/rajware"),
+    $ImagePlatforms = (property IMAGE_PLATFORMS "linux/amd64,linux/arm64,linux/ppc64le,linux/s390x")
+)
+
+$VersionString = "$($VersionMajor).$($VersionMinor).$($BuildNumber)$($PatchString)"
+$ImageName = "$($RegistryUser)/expensetracker-go"
+$ImageTag = "$($ImageName):$($VersionString)"
+$ImageTagLatest = "$($ImageName):latest"
+
