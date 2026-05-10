c29ad932de12a2985ef2dde9d6bd71c77d7ba99f
Raj Chaudhuri <rajch@hotmail.com>
2026-05-10 22:44:19 +0530

    Version 1.0.0

--- invoke.build.ps1
+++ invoke.build.ps1
@@ -2,7 +2,7 @@ Param(
     $VersionMajor   = (property VERSION_MAJOR "1"),
     $VersionMinor   = (property VERSION_MINOR "0"),
     $BuildNumber    = (property BUILD_NUMBER  "0"),
-    $PatchString    = (property PATCH_NUMBER  "-alpha1"),
+    $PatchString    = (property PATCH_NUMBER  ""),
     $RegistryUser   = (property REGISTRY_USER "quay.io/rajware"),
     $ImagePlatforms = (property IMAGE_PLATFORMS "linux/amd64,linux/arm64,linux/ppc64le,linux/s390x"),
     $AddLatestTag   = $true
